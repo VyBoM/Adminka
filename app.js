df3a9ef53f441d438dea35351768d7041aba2638
VyBoM <slava2006.150@gmail.com>
2024-04-23 18:46:58 +1000

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const fs = require("fs").promises;
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+	fs.readFile("./public/index.html", "utf-8").then((data) => {
+		res.header("Content-Type", "text/html");
+		res.send(data);
+	});
+});
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.listen(PORT, () => {
+	console.log(`Приложение запущено тут: http://localhost:${PORT}`);
+});
